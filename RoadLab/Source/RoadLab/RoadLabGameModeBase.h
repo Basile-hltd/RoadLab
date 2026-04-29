a16943f7f481bc9fa1f67070f7a27a590f12aec6
Basile Hiltpold <basile.hiltpold@ludis-software.ch>
2026-04-29 10:55:36 +0200

    Initial UE5 project setup

---
+++ RoadLab/Source/RoadLab/RoadLabGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoadLabGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROADLAB_API ARoadLabGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
