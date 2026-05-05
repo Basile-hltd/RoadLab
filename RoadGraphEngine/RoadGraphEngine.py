ba910495ba870ab578b2e63e2b9fc5f32be73060
Basile Hiltpold <basile.hiltpold@ludis-software.ch>
2026-05-06 00:16:30 +0200

    C'est sympa mais il faut passer a overpass

--- RoadGraphEngine/RoadGraphEngine.py
+++ RoadGraphEngine/RoadGraphEngine.py
@@ -1,3 +1,34 @@
 import sys
+import OSM_Access.OSM_Access as OSM_Access
 
 arg_list = sys.argv
+
+if len(arg_list) != 4:
+    print("-- Invalid argument list --")
+    exit()
+
+print("=== Program Start ===")
+
+try:
+    lat = float(arg_list[1])
+    lon = float(arg_list[2])
+    dia = float(arg_list[3])
+
+except:
+    print("-- Invalid argument list --")
+    exit()
+
+print("Latitude \t:", lat, "\t", type(lat))
+print("Longitude \t:", lon, "\t", type(lon))
+print("Diameter \t:", dia, "\t", type(dia))
+
+api_version = OSM_Access.Version()
+
+if api_version == None:
+    print("-- API error --")
+    exit()
+
+print("\nOpen Street Map API reachable\t Version :", api_version)
+
+if(not OSM_Access.GetTile(lat, lon, 1000)):
+   print("GetTileError")
