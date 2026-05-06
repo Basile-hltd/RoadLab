09f2ab2a0f185ea75259494d7a1c2b973a1b0f85
Basile Hiltpold <basile.hiltpold@ludis-software.ch>
2026-05-06 17:08:14 +0200

    J'ai bcp trop a apprendre de ce moteur je sais pas pour qui je me suis pris la

---
+++ RoadLab/Source/RoadLab/Public/RoadGraphEngineAcess.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MyPythonProcess.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class ROADLAB_API RoadGraphEngineAcess : public UObject {
+	
+	GENERATED_BODY()
+
+public:
+	RoadGraphEngineAcess();
+	~RoadGraphEngineAcess();
+
+};
